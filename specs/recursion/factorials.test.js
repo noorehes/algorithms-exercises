a5cfe10dc4a5510fba9b284a194bf65aa7f76964
noorehes <115871780+noorehes@users.noreply.github.com>
2024-10-06 00:17:50 +0300

    mySol factorials.test.js

--- specs/recursion/factorials.test.js
+++ specs/recursion/factorials.test.js
@@ -10,7 +10,14 @@
   factorial(3) = 6 
 */
 
-function factorial(n) {}
+function factorial(n) {
+  if(n < 2)
+    return 1
+  else {
+    n = n * factorial(n-1)
+  }
+  return n;
+}
 
 // unit tests
 // do not modify the below code
