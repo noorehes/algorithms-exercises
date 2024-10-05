7b4eb27f2a75947e5b83e20fca450a9d393851be
noorehes <115871780+noorehes@users.noreply.github.com>
2024-10-05 23:56:04 +0300

    firstRecursion nested-arrays.test.js

--- specs/recursion/nested-arrays.test.js
+++ specs/recursion/nested-arrays.test.js
@@ -10,7 +10,14 @@
  */
 
 function nestedAdd(array) {
-  // write code here
+  let sum = 0;
+  for (let i = 0; i < array.length; i++){
+    const current = array[i];
+    if(Array.isArray(current)){
+      sum += nestedAdd(current)
+    } else sum += current
+  }
+  return sum;
 }
 
 test.skip("nested arrays addition", () => {
