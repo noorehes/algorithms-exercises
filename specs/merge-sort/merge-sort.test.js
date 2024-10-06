31943d624d9bf3387b8f05dfd73d32762476be9b
noorehes <115871780+noorehes@users.noreply.github.com>
2024-10-06 21:12:23 +0300

    first merge-sort.test.js

--- specs/merge-sort/merge-sort.test.js
+++ specs/merge-sort/merge-sort.test.js
@@ -7,7 +7,29 @@
 */
 
 const mergeSort = (nums) => {
-  // code goes here
+  if(nums.length < 2)
+    return nums;
+
+  const length = nums.length;
+  const middle = Math.floor(length / 2);
+  const right = nums.slice(middle);
+  const left = nums.slice(0, middle);
+
+  const sortedLeft = mergeSort(left);
+  const sortedRight = mergeSort(right);
+
+  return merge(sortedLeft, sortedRight);
+};
+
+const merge = (left, right) => {
+  const results = [];
+
+  while (left.length && right.length){
+    if (left[0] <= right [0])
+      results.push(left.shift()); 
+    else results.push(right.shift());
+  }
+  return results.concat(left , right);
 };
 
 // unit tests
